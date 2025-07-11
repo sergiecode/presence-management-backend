dbd461da748a9025cdc122c8f6c4a397fb175368
Fabri <fabigus14@gmail.com>
2025-07-11 14:08:27 -0300

    Actualizar configuración de CORS para permitir orígenes adicionales

--- cmd/main.go
+++ cmd/main.go
@@ -107,7 +107,7 @@ func main() {
 
 	// CORS middleware - must be before routes
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8081"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8081", "http://localhost:3001"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
